5130b71e96d4158c933a7a1f74d2e5676cbdfb5a
Muhammad Yousuf <84890859+MuhammadYousufbhimla@users.noreply.github.com>
2022-10-20 21:13:24 +0500

    Working

--- connection.php
+++ connection.php
@@ -3,3 +3,14 @@
 $conn =mysqli_connect('localhost','root','','moneytree') or die ("connection Failed");
 
 ?>  
+
+<?php
+
+$conn =mysqli_connect('localhost','root','','moneytree') or die ("connection Failed");
+
+?>  
+<?php
+
+$conn =mysqli_connect('localhost','root','','moneytree') or die ("connection Failed");
+
+?>  
